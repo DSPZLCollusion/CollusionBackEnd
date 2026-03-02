e149f61d4703330d7c79c6e27311d8835393863a
JJue888 <joshuajue888@gmail.com>
2026-03-01 21:10:34 -0800

    create user table

---
+++ src/main/resources/db/migration/create_user_table.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users
+(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name  VARCHAR(50) NOT NULL,
+    email      VARCHAR(62) NOT NULL UNIQUE,
+    password_salt VARCHAR NOT NULL,
+    password_hash VARCHAR NOT NULL
+);
