b6eaf8ed30d60b72ec8ece311462f3873b548f2b
JJue888 <joshuajue888@gmail.com>
2026-03-05 01:44:15 -0800

    naming

--- src/main/resources/db/migration/V2__create_table_roles.sql
+++ src/main/resources/db/migration/V2__create_table_roles.sql
@@ -3,4 +3,9 @@ DROP TABLE IF EXISTS roles CASCADE;
 CREATE TABLE roles (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     role_name VARCHAR(50)
-)
+);
+
+INSERT INTO roles (role_name) VALUES
+                                    ('ADMIN'),
+                                    ('DIRECTOR'),
+                                    ('USER')
