d336ab83c48527d8473d124759b876dfa25f3a4c
JJue888 <joshuajue888@gmail.com>
2026-03-05 01:51:09 -0800

    userroles stored procs

--- src/main/resources/db/migration/V3__create_table_userRoles.sql
+++ src/main/resources/db/migration/V3__create_table_userRoles.sql
@@ -1,9 +1,10 @@
-DROP TABLE IF EXISTS userRoles CASCADE;
+DROP TABLE IF EXISTS user_roles CASCADE;
 
-CREATE TABLE userRoles (
+CREATE TABLE user_roles (
     user_id BIGINT NOT NULL ,
     role_id BIGINT NOT NULL,
     assigned_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    assigned_by BIGINT        NULL,
 
     PRIMARY KEY (user_id, role_id),
 
