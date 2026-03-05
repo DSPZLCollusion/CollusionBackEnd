b0dd79a2421535c66ff26eafc16e52c0710170b2
JJue888 <joshuajue888@gmail.com>
2026-03-05 01:22:25 -0800

    table scripts

---
+++ src/main/resources/db/migration/V3__create_table_userRoles.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS userRoles CASCADE;
+
+CREATE TABLE userRoles (
+    user_id BIGINT NOT NULL ,
+    role_id BIGINT NOT NULL,
+    assigned_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+
+    PRIMARY KEY (user_id, role_id),
+
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+            REFERENCES users(id)
+            ON DELETE CASCADE,
+
+    CONSTRAINT fk_role
+        FOREIGN KEY(role_id)
+            REFERENCES roles(id)
+            ON DELETE CASCADE
+)
