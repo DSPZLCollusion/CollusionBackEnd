71b5a2ba40c965c69c2839df2e13cf111787ddea
JJue888 <joshuajue888@gmail.com>
2026-03-07 01:47:45 -0800

    role files

--- src/main/java/com/collusion/api/domain/user/Role.java
+++ src/main/java/com/collusion/api/domain/user/Role.java
@@ -1,4 +1,20 @@
 package com.collusion.api.domain.user;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@NoArgsConstructor
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Column(name = "role_name", nullable = false, unique = true, length = 50)
+    private String roleName;
 }
