1cea2dc021155336a6458c357e92cbbc87e41266
JJue888 <joshuajue888@gmail.com>
2026-05-07 23:11:34 -0400

    Models for login/tokens

---
+++ src/main/java/com/collusion/api/models/ERole.java
@@ -0,0 +1,7 @@
+package com.collusion.api.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_DIRECTOR,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/collusion/api/models/User.java
@@ -0,0 +1,55 @@
+package com.collusion.api.models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "email")
+        })
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 50)
+    private String lastName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(  name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+}
